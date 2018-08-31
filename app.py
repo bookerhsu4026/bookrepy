81247e3a1a2070000c11dc5b1a065f23e0e38997
bookerhsu4026 <42689093+bookerhsu4026@users.noreply.github.com>
2018-08-31 21:49:54 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def handle_message(event):
 
     print('uid: '+uid)
     print('name:'+nameid)
-    print('is_buy:'+is_buy)
+    print('is_buy:'+str(is_buy))
 
     # 傳送圖片
     if event.message.text == '買東西':
